ff73a79ad74cab6903b673cc05c503a2f3d70325
marharyta zozulia <zozulia.marharyta@gmail.com>
2025-05-28 14:24:28 +0200

    js

---
+++ js/online-lessons.js
@@ -0,0 +1,23 @@
+// function foo(a, b, c, d, e) {
+//     console.log(arguments);
+//     const arr = array.from(arguments);
+//     console.log(arr);
+// }
+
+
+
+
+// foo(1, 2, 3)
+// foo(10,11,12,13,14)
+
+
+
+
+// const add = function (){
+//     console.log("add")
+// }
+// add()
+
+
+
+
