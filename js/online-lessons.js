e7e5ce5a920bb3f7a07a57bba961f184ae93801c
marharyta zozulia <zozulia.marharyta@gmail.com>
2025-06-01 14:13:41 +0200

    js

--- js/online-lessons.js
+++ js/online-lessons.js
@@ -155,3 +155,39 @@ function calculateSum(numbers) {
     return sumArray;
 
 }
+
+
+
+
+
+
+// Напишіть функцію calcTotalPrice(fruits, fruitName),
+// яка приймає массив об'єктів (fruits) і рядок з назвою фрукта (fruitName).
+// Функція рахує і повертає загальну вартість фрукта
+// з таким ім'ям, ціною та кількістю з об'єкта.
+
+// Зверніть увагу, що в масиві може бути кілька обʼєктів з однаковою 
+// назвою фрукта, це також треба урахувати.
+
+const fruits = [
+    { name: "Яблуко", price: 45, quantity: 7 },
+    { name: "Апельсин", price: 60, quantity: 4 },
+    { name: "Банан", price: 125, quantity: 8 },
+    { name: "Груша", price: 350, quantity: 2 },
+    { name: "Виноград", price: 440, quantity: 3 },
+    { name: "Банан", price: 125, quantity: 3 },
+  ];
+
+
+console.log(calcTotalPrice(fruits,"Виноград"))
+
+function calcTotalPrice(fruits, fruitName) {
+    let totalPrice = 0;
+    for (const fruit of fruits) {
+        if (fruit.name === fruitName) {
+            totalPrice += fruit.Price * fruit.quantity;
+        }
+    }
+
+    return totalPrice;
+}
